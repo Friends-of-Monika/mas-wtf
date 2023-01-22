9f2bfd38b8e60230f0486b7796b8daf4bc985a47
dreamscached <dreamscache.d@gmail.com>
2023-01-22 17:38:49 +0100

    Include LICENSE in built package

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -24,6 +24,7 @@ Copy-Item -Recurse $Dir\mod $Mod
 New-Item -ItemType Directory -Force -Path $Mod\lib\unrpyc | Out-Null
 Copy-Item -Recurse $Dir\lib\unrpyc\decompiler $Mod\lib\unrpyc\decompiler
 Copy-Item $Dir\lib\unrpyc\*.py $Mod\lib\unrpyc
+Copy-Item $Dir\lib\unrpyc\LICENSE.txt $Mod\lib\unrpyc
 
 Compress-Archive -Update -Path $Temp\game -DestinationPath $Build\$Package-$Version.zip
 Remove-Item -Recurse $Temp
--- scripts/build.sh
+++ scripts/build.sh
@@ -17,5 +17,6 @@ cp -r "$dir/mod"/* "$mod"
 mkdir -p "$mod/lib/unrpyc"
 cp -r "$dir/lib/unrpyc/decompiler" "$mod/lib/unrpyc/decompiler"
 cp "$dir/lib/unrpyc/"*.py "$mod/lib/unrpyc"
+cp "$dir/lib/unrpyc/LICENSE.txt" "$mod/lib/unrpyc"
 
 (cd "$temp" || exit 1; find game | zip -9@q "$build/$package-$version.zip" && rm -rf "$temp")
